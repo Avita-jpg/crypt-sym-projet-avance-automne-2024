0de68c90a090091d489f5048e1edfde97b40fda1
Aylweenn <eve.bollengier@gmail.com>
2024-10-08 16:28:20 +0200

    chiffrage_xor.c création de la fonction chiffrageXor

--- Code C/chiffrage_xor.c
+++ Code C/chiffrage_xor.c
@@ -2,3 +2,19 @@
 #include <stdio.h>  // Pour l'utilisation des fonctions d'entrée/sortie comme printf
 #include <stdlib.h> // Pour les fonctions de gestion de la mémoire et utilitaires comme malloc, free
 #include <string.h> // Pour manipuler les chaînes de caractères (strlen, etc.)
+
+int inc(int nombre, int nbMod){
+    return (nombre+1) % nbMod;
+}
+
+
+char * chiffrageXor(unsigned char * msg, unsigned char * key){
+    int taille = strlen(msg);
+    unsigned char msg_chiffre[taille] ;
+
+    for (int i=0, j=0; i < taille; i++){
+        msg_chiffre[i] = msg[i] ^ key[j];
+        j = inc(j, strlen(key));
+    }
+    return msg_chiffre;
+}
--- Code C/chiffrage_xor.h
+++ Code C/chiffrage_xor.h
@@ -6,6 +6,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+//fonction qui incrémente à l'aide d'un modulo nbMod une variable nombre
+int inc(int nombre, int nbMod);
+
+// Fonction qui permet de chiffrer et de déchiffrer un message caractère par caractère à l'aide d'un XOR
+char * chiffrageXor(unsigned char * msg, unsigned char * key);
+
 
 
 
