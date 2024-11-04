3233a37ed93fe8b335d1e1801e0659a7391fb64a
Aylweenn <eve.bollengier@gmail.com>
2024-11-04 19:14:51 +0100

    masques_jetables et les fonctions pour tout tester

--- Code C/test_chiffrage_xor.c
+++ Code C/test_chiffrage_xor.c
@@ -5,68 +5,38 @@
 
 
 int main(void){
-    printf("---------------- Test fonction XOR chiffrement/déchiffrement ---------------- \n\n ");
+
+    printf("---------------- Tests masque jetable ---------------- \n\n ");
+    //test sur des textes
+    tests_masque_jetable("../script/CRYPT/tests/ref/msg2.txt");
+    tests_masque_jetable("../script/CRYPT/tests/ref/bovary-isolatin1.txt");
+
+    printf("Test sur un fichier avec des accents \n");
+    tests_masque_jetable("../script/CRYPT/tests/ref/moyen.txt");
+
+    tests_masque_jetable("../script/CRYPT/tests/ref/chinois-utf-8.rtf");
+
+    //test sur une image
+    tests_masque_jetable("../script/CRYPT/tests/ref/chaton.jpg");
+
+
+    printf("---------------- Tests fonction XOR chiffrement/déchiffrement ---------------- \n\n ");
 
     // Clé qui va permettre le chiffrement
-    char* key = "rutabaga";
-    unsigned char *key_unsigned = (unsigned char *)key;
-    unsigned char *message;
-    long taille_fichier;
+    char *key = "rutabaga";
+    //char *key = "df9";
+    //char *key = "adijd";
+    //char *key = "DfklEa_";
 
-    // Ouverture du fichier à chiffrer
-    FILE* fich = NULL; 
+    //test sur des textes
+    test_chiffrage_xor("../script/CRYPT/tests/ref/msg2.txt", key);
+    test_chiffrage_xor("../script/CRYPT/tests/ref/bovary-isolatin1.txt", key);
+    test_chiffrage_xor("../script/CRYPT/tests/ref/moyen.txt", key);
+    test_chiffrage_xor("../script/CRYPT/tests/ref/chinois-utf-8.rtf", key);
 
-    // On ouvre le fichier avec le message que l'on doit chiffrer
-    fich  = fopen("../script/CRYPT/tests/ref/msg2.txt", "rb");
-    if (fich == NULL) {
-        perror("Erreur lors de l'ouverture du fichier");
-        exit(1);
-    }
-
-    //On prends la taille du message dans le fichier
-    fseek(fich, 0, SEEK_END);
-    taille_fichier = ftell(fich);  // Obtenir la taille du fichier
-    rewind(fich);  // Remettre le curseur au début du fichier
-
-    // Allouer suffisamment de mémoire pour stocker tout le fichier
-    message = (unsigned char*) malloc(sizeof(unsigned char) * taille_fichier + 1);
-    if (message == NULL) {
-        perror("Erreur allocation mémoire");
-        fclose(fich);
-        exit(1);
-    }
-
-    // Lecture dans le fichier et on met ça dans une variable unsigned char*
-    if(fread(message, 1, taille_fichier, fich) != (size_t)taille_fichier){
-        perror("Erreur lecture fichier : ");
-        free(message);
-        fclose(fich);
-        exit(1);
-    }
-
-    message[taille_fichier] = '\0'; 
-
-    // On chiffre ensuite le message
-    unsigned char * msg_chiffre = chiffrageXor(message, key_unsigned, taille_fichier);
-
-    // On déchiffre le message
-    unsigned char *  msg_clair = chiffrageXor(msg_chiffre, key_unsigned,taille_fichier); //on doit retomber sur le message de base
-
-    // On 
-    if(memcmp(message, msg_clair, taille_fichier) == 0){
-        printf("Same content \n");
-    }
-    else{
-        printf("Not the same content \n");
-    }
-
-    // On libère la mémoire et on ferme les fichiers
-    // free(hex_contenu);
-    // free(hex_result_final);
-    free(message);
-    free(msg_chiffre);
-    free(msg_clair);
-    fclose(fich);
+    //test sur une image
+    test_chiffrage_xor("../script/CRYPT/tests/ref/chaton.jpg", key);
+    
 
     return 0;
 }
