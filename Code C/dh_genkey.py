39a93d6849430a98e3ada54e298a404ac1e3dbff
Andjelka27 <anjeli2727@gmail.com>
2024-11-26 14:51:21 +0100

    ajout final de la partie 2, tout fonctionne maintenant

--- Code C/dh_genkey.py
+++ Code C/dh_genkey.py
@@ -16,7 +16,7 @@ def Alice(p,g,queueAlice,queueBob,queueCle):
    print(f"\n[Connu d'Alice seulement] --> a = {a}")
 
    A = puissance_mod_n (g,a,p)
-   print(f"\n[Connu d'Alice, Bob et de potentiels espions(Eve)] --> A = {A}")
+   print(f"\n[Connu d'Alice, Bob et de potentiels espions(Eve)] --> A (g^a[p]) = {A}")
    queueAlice.put(A)
    
    while queueBob.empty() : # tant que Bob n'ajoute pas B à sa file, on attend
@@ -34,7 +34,7 @@ def Bob(p,g,queueAlice,queueBob):
    print(f"\n[Connu de Bob seulement] --> b = {b}")
 
    B = puissance_mod_n (g,b,p)
-   print(f"\n[Connu d'Alice, Bob et de potentiels espions(Eve)] --> B = {B}")
+   print(f"\n[Connu d'Alice, Bob et de potentiels espions(Eve)] --> B (g^b[p]) = {B}")
    queueBob.put(B)
 
    while queueAlice.empty() : 
@@ -49,10 +49,10 @@ def Bob(p,g,queueAlice,queueBob):
 def main() :
   
   parser = argparse.ArgumentParser(description="Génération de la clé de Diffie-Hellman.")
-  parser.add_argument('-i', '--input', type=str, required=True, help='Fichier d\'entrée avec les paramètres p et g.')
-  parser.add_argument('-o', '--output', type=str, required=True, help='Fichier de sortie pour la clé générée.')
+  parser.add_argument('-i', '--input', type=str, required=True, help='Fichier d\'entrée avec les paramètres p et g.') # le fichier d'entrée avec les arguments
+  parser.add_argument('-o', '--output', type=str, required=True, help='Fichier de sortie pour la clé générée.') # le fichier de sortie qui aura la clé
 
-  args = parser.parse_args() # 
+  args = parser.parse_args() 
 
 
   with open(args.input, 'r') as file: # lecture fichier mis in input -i
@@ -62,9 +62,9 @@ def main() :
   print(f"p : {p}")
   print(f"g : {g}")
 
-  queueAlice = queue.Queue()  # file d'attente pour A
-  queueBob = queue.Queue()  # file d'attente pour B
-  queueCle = queue.Queue() # file d'attente pour la clé
+  queueAlice = queue.Queue()  # file d'attente pour A (g^a[p]) 
+  queueBob = queue.Queue()  # file d'attente pour B (g^b[p]) 
+  queueCle = queue.Queue() # file d'attente pour la clé finale
 
   t1=threading.Thread(target=Alice,args=(p,g,queueAlice,queueBob,queueCle,)) # création du thread de la fonction alice avec les arguments donnés
   t2=threading.Thread(target=Bob,args=(p,g,queueAlice,queueBob,))
